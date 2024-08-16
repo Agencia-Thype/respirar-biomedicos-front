3235f0db4d963e9a10f04dd9e2b6caf7f48921e5
thyemimello <thyemi_mello@hotmail.com>
2024-08-16 12:12:01 -0300

    home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,8 @@ import {
   Flex,
   Heading,
   Image,
+  LinkBox,
+  LinkOverlay,
   SimpleGrid,
   Text,
 } from "@chakra-ui/react";
@@ -25,43 +27,6 @@ export const HomePage = () => {
     <Box>
       <Header />
       <Slider />
-
-      <Flex bg="#E9F1F5" padding={"8%"}>
-        <Container maxW={"7xl"}>
-          <Flex
-            flexDir={{ base: "column", md: "row" }}
-            mt="2rem"
-            justify={"space-around"}
-            alignItems={"center"}
-            gap="3rem"
-            // h={"100%"}
-          >
-            <Flex flexDir={"column"} align={"flex-start"} gap="35px" w="100%">
-              <Heading color={"#116CA0"}>Sobre nós</Heading>
-              <Text color={"#116CA0"} textAlign={"justify"}>
-                A RESPIRAR PRUDUTOS BIOMEDICOS Ltda. é a resposta à crescente
-                demanda por serviços de venda e aluguel de equipamentos médicos
-                para que o paciente possa continuar seu tratamento em seu
-                ambiente natural, em casa.
-              </Text>
-              <Button w={"240px"} bg={"#116CA0"} color={"#FFFFFF"}>
-                Saiba Mais
-              </Button>
-            </Flex>
-            <Flex
-              flexDir={"column"}
-              align={"center"}
-              border="5px solid #116CA0"
-              gap="0.5rem"
-              w="100%"
-              borderRadius={"12px"}
-              overflow={"hidden"}
-            >
-              <Image src={lifestyleImg} />
-            </Flex>
-          </Flex>
-        </Container>
-      </Flex>
 
       <Flex padding={"8%"}>
         <Container maxW={"7xl"}>
@@ -129,6 +94,59 @@ export const HomePage = () => {
           </Flex>
         </Container>
       </Flex>
+
+      <Flex bg="#E9F1F5" padding={"8%"}>
+        <Container maxW={"7xl"}>
+          <Flex
+            flexDir={{ base: "column", md: "row" }}
+            mt="2rem"
+            justify={"space-around"}
+            alignItems={"center"}
+            gap="3rem"
+            // h={"100%"}
+          >
+            <Flex flexDir={"column"} align={"flex-start"} gap="35px" w="100%">
+              <Heading color={"#116CA0"}>Sobre nós</Heading>
+              <Text color={"#116CA0"} textAlign={"justify"}>
+                A RESPIRAR PRUDUTOS BIOMEDICOS Ltda. é a resposta à crescente
+                demanda por serviços de venda e aluguel de equipamentos médicos
+                para que o paciente possa continuar seu tratamento em seu
+                ambiente natural, em casa.
+              </Text>
+              <LinkBox
+                  as="article"
+                  bg="#116CA0"
+                  color="#FFFFFF"
+                  w={"240px"}
+                  borderRadius="md"
+                  display={"flex"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  fontSize={"16.8px"}
+                  fontWeight="bold"
+                  padding="8px 16px"
+                  _hover={{ bg: "gray.200" }}
+                >
+                  <LinkOverlay href="/contacts">SAIBA MAIS</LinkOverlay>
+                </LinkBox>
+             
+            </Flex>
+            <Flex
+              flexDir={"column"}
+              align={"center"}
+              border="5px solid #116CA0"
+              gap="0.5rem"
+              w="100%"
+              borderRadius={"12px"}
+              overflow={"hidden"}
+            >
+              <Image src={lifestyleImg} />
+            </Flex>
+          </Flex>
+        </Container>
+      </Flex>
+
+      
       <Footer />
     </Box>
   );
