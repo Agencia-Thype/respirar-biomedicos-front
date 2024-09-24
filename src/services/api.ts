ddf9ca51b1292b7acf9d16e5d65219151d61fa6d
Thyemi de Mello <104704367+thyemimello@users.noreply.github.com>
2024-09-24 14:08:59 -0300

    Update api.ts
    alterado para 50000

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
 
-   //baseURL: "http://127.0.0.1:5000",
-  baseURL: "https://respirar-backend.onrender.com",
+   baseURL: "http://127.0.0.1:50000",
+  //baseURL: "https://respirar-backend.onrender.com",
 
 });
