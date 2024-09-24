cb49e40ceec5ba77deda1b513ab3a21d5d049abf
Thyemi de Mello <104704367+thyemimello@users.noreply.github.com>
2024-09-24 13:50:24 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
 
-   baseURL: "http://127.0.0.1:5000",
-  //baseURL: "https://respirar-backend.onrender.com",
- // baseURL: "https://api.render.com/deploy/srv-cron05qj1k6c739km4g0?key=_DMii2mISmA"
+   //baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://respirar-backend.onrender.com",
+
 });
