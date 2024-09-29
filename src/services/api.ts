469d8d3462dad7b0c84c07b4e8d9cfde29e015f4
Thyemi de Mello <104704367+thyemimello@users.noreply.github.com>
2024-09-28 22:29:49 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-// baseURL: "https://respirar-backend.onrender.com"
 baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:5000",
 });
