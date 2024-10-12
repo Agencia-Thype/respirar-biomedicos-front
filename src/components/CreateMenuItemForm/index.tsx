6330ce4af13342e89b7e0ad64ceae5f0a7f249c7
angelobarilari <98756174+angelobarilari@users.noreply.github.com>
2024-10-12 20:33:35 -0300

    fix: foi corrigido o envio de imagens, agora podemos adicionar e remover diversas imagens na criaçao de itens

--- src/components/CreateMenuItemForm/index.tsx
+++ src/components/CreateMenuItemForm/index.tsx
@@ -1,22 +1,27 @@
 import React, { useState, useContext } from "react";
 import {
-  Button,
-  Checkbox,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-  Select,
-  Textarea,
-  VStack,
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
+    Select,
+    Textarea,
+    VStack,
+    Text,
+    Image,
+    IconButton,
+    Box,
 } from "@chakra-ui/react";
+import { IoMdClose } from "react-icons/io";
+// import { CloseIcon } from "@chakra-ui/icons";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createMenuItemSchema } from "../../schemas/menuItem.schemas";
 import { MenuItemContext } from "../../contexts/MenuItemContext";
 import { CategoriesContext } from "../../contexts/CategoriesContext";
-import { IMenuItemCreate } from "../../interfaces/menuItem.interfaces";
 
 interface ICreateMenuItem {
     name: string;
@@ -26,143 +31,190 @@ interface ICreateMenuItem {
     categoryId: string;
     sale: boolean;
     featuredProduct: boolean;
-    images: File[]
-  }
+    images: File[];
+}
+
 export const CreateMenuItemForm = () => {
-  const { data: categories, isFetching } = useContext(CategoriesContext);
-  const { createMenuItem } = useContext(MenuItemContext);
+    const { data: categories, isFetching } = useContext(CategoriesContext);
+    const { createMenuItem } = useContext(MenuItemContext);
 
-  const [price, setPrice] = useState<string>("");
-  const [productImages, setProductImages] = useState<File[]>([]); // Para lidar com arquivos
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ICreateMenuItem>({
-    resolver: zodResolver(createMenuItemSchema),
-  });
-  console.log(errors);
+    const [productImages, setProductImages] = useState<File[]>([]);
+    const [imageError, setImageError] = useState<string | null>(null);
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    
-    if (e.target.files) {
-      // Certifique-se de que productImages seja sempre um array de arquivos
-      const selectedFiles = Array.from(e.target.files);
-      // const selectedFiles: File[] = Array.from(e.target.files);
-      // console.log("linha 51 " + selectedFiles)
-      // Create a new File object
-      const file1 = new File(["content of file 1"], "file1.txt", { type: "text/plain" });
-      const file2 = new File(["content of file 2"], "file2.txt", { type: "text/plain" });
-
-      // Create an array of files
-      // const selectedFiles: File[] = [file1, file2];
-      const files = [...e.target.files]
-      
-      console.log(typeof(files))
-      setProductImages(selectedFiles);  // Agora você substitui os arquivos em vez de adicionar ao array
-    }
-  };
-  const onSubmit: SubmitHandler<ICreateMenuItem> = async (data) => {
-    console.log("Formulário Enviado");
-    
-    const imagesArray = Array.isArray(productImages) ? productImages : [];
-    
-    const formData = new FormData();
-    
-    // Adicionar arquivos ao FormData
-    imagesArray.forEach((file, index) => {
-      formData.append(`images[${index}]`, file);
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState: { errors },
+    } = useForm<ICreateMenuItem>({
+        resolver: zodResolver(createMenuItemSchema),
     });
-  
-    // Adicionar os outros dados
-    formData.append("name", data.name);
-    formData.append("price", data.price.toString().replace(",", "."));  // Certifique-se de que o preço seja um número válido
-    formData.append("resume", data.resume);
-    formData.append("description", data.description);
-    formData.append("categoryId", data.categoryId);
-    formData.append("sale", data.sale ? "true" : "false");
-    formData.append("featuredProduct", data.featuredProduct ? "true" : "false");
-  
-    console.log(formData);
-    
-    // try {
-    //   // Enviar os dados via Axios ou outro cliente HTTP
-    // } catch (error) {
-    //   console.error("Erro ao criar item de menu", error);
-    // }
-  };
-  
-  
 
-  return (
-    <Flex as="form" flexDir={"column"} onSubmit={handleSubmit(onSubmit)} gap={"2rem"} encType="multipart/form-data">
-      <VStack spacing={6} w="100%">
-        <FormControl isInvalid={!!errors.name}>
-          <FormLabel>Nome</FormLabel>
-          <Input {...register("name")} placeholder="Digite o nome do produto" />
-          {!!errors.name && <FormErrorMessage>{errors.name.message}</FormErrorMessage>}
-        </FormControl>
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            const selectedFiles = Array.from(e.target.files);
+            const totalImages = productImages.length + selectedFiles.length;
 
-        <FormControl isInvalid={!!errors.price}>
-          <FormLabel>Preço</FormLabel>
-          <Input
-            {...register("price")}
-            placeholder="Digite o preço do produto"
-            onChange={(e) => setPrice(e.target.value)}
-            value={price}
-            type="text"
-          />
-          {!!errors.price && <FormErrorMessage>{errors.price.message}</FormErrorMessage>}
-        </FormControl>
+            if (totalImages > 5) {
+                setImageError("Você pode adicionar no máximo 5 imagens.");
+                return;
+            }
 
-        <FormControl isInvalid={!!errors.resume}>
-          <FormLabel>Resumo</FormLabel>
-          <Textarea {...register("resume")} placeholder="Digite o resumo do produto" />
-          {!!errors.resume && <FormErrorMessage>{errors.resume.message}</FormErrorMessage>}
-        </FormControl>
+            const updatedImages = [...productImages, ...selectedFiles];
 
-        <FormControl isInvalid={!!errors.description}>
-          <FormLabel>Descrição</FormLabel>
-          <Textarea {...register("description")} placeholder="Digite a descrição do produto" />
-          {!!errors.description && <FormErrorMessage>{errors.description.message}</FormErrorMessage>}
-        </FormControl>
+            setProductImages(updatedImages);
+            setValue("images", updatedImages);
+            setImageError(null);
+        }
+    };
 
-        <FormControl>
-          <FormLabel>Imagens do Produto</FormLabel>
-          <Input
-            type="file"
-            multiple 
-            onChange={handleImageChange}
-            accept="image/*"
-          />
-          
-          {/* {!!errors.resume && <FormErrorMessage>{errors.resume.message}</FormErrorMessage>} */}
-        </FormControl>
+    const removeImage = (index: number) => {
+        const updatedImages = productImages.filter((_, i) => i !== index);
+        setProductImages(updatedImages);
+        setValue("images", updatedImages);
+    };
 
-        <FormControl>
-          <FormLabel>Categoria</FormLabel>
-          <Select {...register("categoryId")}>
-            <option value="">Selecione uma categoria</option>
-            {categories?.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </Select>
-        </FormControl>
+    const onSubmit: SubmitHandler<ICreateMenuItem> = async (data) => {
+        if (productImages.length === 0) {
+            setImageError("Você deve adicionar pelo menos uma imagem.");
+            return;
+        }
 
-        <FormControl>
-          <Checkbox {...register("sale")}>Em Promoção</Checkbox>
-        </FormControl>
+        const fetchedImages: [File, ...File[]] = productImages as [
+            File,
+            ...File[]
+        ];
 
-        <FormControl>
-          <Checkbox {...register("featuredProduct")}>Produto em Destaque</Checkbox>
-        </FormControl>
+        const fetchedData = {
+            ...data,
+            images: fetchedImages,
+        };
 
-        <Button type="submit" colorScheme="blue">
-          Cadastrar Produto
-        </Button>
-      </VStack>
-    </Flex>
-  );
+        createMenuItem(fetchedData);
+    };
+
+    return (
+        <Flex
+            as="form"
+            flexDir={"column"}
+            onSubmit={handleSubmit(onSubmit)}
+            gap={"2rem"}
+            encType="multipart/form-data"
+        >
+            <VStack spacing={6} w="100%">
+                <FormControl isInvalid={!!errors.name}>
+                    <FormLabel>Nome</FormLabel>
+                    <Input
+                        {...register("name")}
+                        placeholder="Digite o nome do produto"
+                    />
+                    {!!errors.name && (
+                        <FormErrorMessage>
+                            {errors.name.message}
+                        </FormErrorMessage>
+                    )}
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.price}>
+                    <FormLabel>Preço</FormLabel>
+                    <Input
+                        {...register("price")}
+                        placeholder="Digite o preço do produto"
+                        type="text"
+                    />
+                    {!!errors.price && (
+                        <FormErrorMessage>
+                            {errors.price.message}
+                        </FormErrorMessage>
+                    )}
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.resume}>
+                    <FormLabel>Resumo</FormLabel>
+                    <Textarea
+                        {...register("resume")}
+                        placeholder="Digite o resumo do produto"
+                    />
+                    {!!errors.resume && (
+                        <FormErrorMessage>
+                            {errors.resume.message}
+                        </FormErrorMessage>
+                    )}
+                </FormControl>
+
+                <FormControl isInvalid={!!errors.description}>
+                    <FormLabel>Descrição</FormLabel>
+                    <Textarea
+                        {...register("description")}
+                        placeholder="Digite a descrição do produto"
+                    />
+                    {!!errors.description && (
+                        <FormErrorMessage>
+                            {errors.description.message}
+                        </FormErrorMessage>
+                    )}
+                </FormControl>
+
+                <FormControl isInvalid={!!imageError}>
+                    <FormLabel>Imagens do Produto (Máx: 5)</FormLabel>
+                    <Input
+                        type="file"
+                        multiple
+                        onChange={handleImageChange}
+                        accept="image/*"
+                    />
+                    {imageError && <Text color="red.500">{imageError}</Text>}
+                </FormControl>
+
+                <Box display="flex" flexWrap="wrap" gap={4}>
+                    {productImages.map((image, index) => (
+                        <Box key={index} position="relative">
+                            <Image
+                                src={URL.createObjectURL(image)}
+                                alt={`Imagem ${index + 1}`}
+                                boxSize="100px"
+                                objectFit="cover"
+                                borderRadius="md"
+                            />
+                            <IconButton
+                                aria-label="Remover imagem"
+                                icon={<IoMdClose />}
+                                size="xs"
+                                position="absolute"
+                                top="0"
+                                right="0"
+                                onClick={() => removeImage(index)}
+                            />
+                        </Box>
+                    ))}
+                </Box>
+
+                <FormControl>
+                    <FormLabel>Categoria</FormLabel>
+                    <Select {...register("categoryId")}>
+                        <option value="">Selecione uma categoria</option>
+                        {categories?.map((category) => (
+                            <option key={category.id} value={category.id}>
+                                {category.name}
+                            </option>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                <FormControl>
+                    <Checkbox {...register("sale")}>Em Promoção</Checkbox>
+                </FormControl>
+
+                <FormControl>
+                    <Checkbox {...register("featuredProduct")}>
+                        Produto em Destaque
+                    </Checkbox>
+                </FormControl>
+
+                <Button type="submit" colorScheme="blue">
+                    Cadastrar Produto
+                </Button>
+            </VStack>
+        </Flex>
+    );
 };
--- src/pages/AddMenuItemPage.tsx
+++ src/pages/AddMenuItemPage.tsx
@@ -5,72 +5,73 @@ import { ProductListSearchProps } from "../interfaces/menuItem.interfaces";
 import { useNavigate } from "react-router-dom";
 import { CreateMenuItemForm } from "../components/CreateMenuItemForm";
 
-
-export const AddMenuItemPage: React.FC<ProductListSearchProps>  = ({filteredCardapio,setFilteredCardapio, handleSearch}) => {
-
+export const AddMenuItemPage: React.FC<ProductListSearchProps> = ({
+    filteredCardapio,
+    setFilteredCardapio,
+    handleSearch,
+}) => {
     const navigate = useNavigate();
     return (
         <Flex flexDir="column" w="100%">
-        <Flex w="100%">
-            <Header handleSearch={handleSearch}  setFilteredCardapio={setFilteredCardapio}/>
-        </Flex>
-        <Heading mt={"3rem"} fontSize={"34px"} padding={"0 10%"}>Bem Vindo</Heading>
-
-
-        <Flex
-            justify={{ base: "flex-start", md: "center" }}
-            align={"center"}
-            mt="2rem"
-            overflowX={"auto"}
-            gap={{ base: "3rem" }}
-            padding={"0 10%"}
-        >
-            <Heading
-            // onClick={toggleCreateModal}
-            cursor={"pointer"}
-            fontFamily={"Montserrat"}
-            fontSize={"22px"}
-            // borderBottom={showSubMenu ? "solid 1px white" : "none"}
-            >
-            Adicionar Produto
+            <Flex w="100%">
+                <Header
+                    handleSearch={handleSearch}
+                    setFilteredCardapio={setFilteredCardapio}
+                />
+            </Flex>
+            <Heading mt={"3rem"} fontSize={"34px"} padding={"0 10%"}>
+                Bem Vindo
             </Heading>
-            <Heading
-            onClick={() => navigate("/orders")}
-            cursor={"pointer"}
-            fontFamily={"Montserrat"}
-            fontSize={"22px"}
+
+            <Flex
+                justify={{ base: "flex-start", md: "center" }}
+                align={"center"}
+                mt="2rem"
+                overflowX={"auto"}
+                gap={{ base: "3rem" }}
+                padding={"0 10%"}
             >
-            Pedidos
-            </Heading>
-            <Heading
-            onClick={() => navigate("/delivery")}
-            cursor={"pointer"}
-            fontFamily={"Montserrat"}
-            fontSize={"22px"}
+                <Heading
+                    // onClick={toggleCreateModal}
+                    cursor={"pointer"}
+                    fontFamily={"Montserrat"}
+                    fontSize={"22px"}
+                    // borderBottom={showSubMenu ? "solid 1px white" : "none"}
+                >
+                    Adicionar Produto
+                </Heading>
+                <Heading
+                    onClick={() => navigate("/orders")}
+                    cursor={"pointer"}
+                    fontFamily={"Montserrat"}
+                    fontSize={"22px"}
+                >
+                    Pedidos
+                </Heading>
+                <Heading
+                    onClick={() => navigate("/delivery")}
+                    cursor={"pointer"}
+                    fontFamily={"Montserrat"}
+                    fontSize={"22px"}
+                >
+                    Entregas
+                </Heading>
+            </Flex>
+
+            <Flex
+                w={"100%"}
+                flexDirection={"column"}
+                padding={"5% 10%"}
+                gap={"3rem"}
             >
-            Entregas
-            </Heading>
-        </Flex>
-
-
-
-
-
-
-        <Flex
-         w={"100%"}
-         flexDirection={"column"}
-         padding={"5% 10%"}
-         gap={"3rem"}
-        >
-            <Heading 
-                textAlign={"center"}
-                size={{ base: "2xl", md: "3xl", lg: "4xl" }}
-            >
-                Novo Produto
-            </Heading>
-            <CreateMenuItemForm />
-        </Flex>
+                <Heading
+                    textAlign={"center"}
+                    size={{ base: "2xl", md: "3xl", lg: "4xl" }}
+                >
+                    Novo Produto
+                </Heading>
+                <CreateMenuItemForm />
+            </Flex>
         </Flex>
     );
 };
